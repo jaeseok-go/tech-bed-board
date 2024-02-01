b1c8d6bd8aca8428cdd88b76347327fcfff9764a
jeeyulee <jeeyu-lee@naver.com>
2024-02-01 23:16:28 +0900

    chore: create basic multi-module architecture

---
+++ tech-bed-apis/src/main/java/js/toy/TechBedApplication.java
@@ -0,0 +1,12 @@
+package js.toy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechBedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechBedApplication.class, args);
+    }
+}
